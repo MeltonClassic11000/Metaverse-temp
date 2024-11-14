9d6d4ee35f86e042f8e9df47f89eef7ddbe62121
Melton Classic <>
2024-11-14 17:18:51 +0500

    counter add

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,29 +8,22 @@ $('.letter').each((i, el) => $(el).css('animation-delay', `${i * 0.1}s`));
 
 setTimeout(() => $('#textContainer').addClass('end'), $('.letter').length * 100);
 
+$(function () {
+  const end = 3000, duration = 1000, increment = 50;
+  const stepTime = duration / (end / increment);
+  const $counter = $('#counter').css('opacity', 0);
 
-$(function() {
-  let counterStarted = false;
-  const $counterSection = $('#counter-section');
-  const $counter = $('#counter');
-  let counterValue = 0;
-
-  $(window).on('scroll', function() {
-      const windowHeight = $(window).height();
-      const sectionTop = $counterSection.offset().top;
-      const sectionBottom = sectionTop + $counterSection.outerHeight();
-      
-      if ($(window).scrollTop() + windowHeight > sectionTop && $(window).scrollTop() < sectionBottom && !counterStarted) {
-          counterStarted = true;
+  new IntersectionObserver((entries, observer) => {
+      if (entries[0].isIntersecting) {
+          $counter.css('opacity', 1);
+          let counter = 0;
           const interval = setInterval(() => {
-              if (counterValue < 3000) {
-                  counterValue += 10; // Increment by 10
-                  $counter.text(counterValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")); // Display with commas
-              } else {
-                  $counter.text("3K"); // Once 3000 is reached, display "3K"
-                  clearInterval(interval);
-              }
-          }, 10); // Speed of the counter increase (10ms interval)
+              counter += increment;
+              $counter.text(counter >= end ? '3k' : counter);
+              if (counter >= end) clearInterval(interval);
+          }, stepTime);
+          observer.disconnect();
       }
-  });
+  }).observe($counter[0]);
 });
+
